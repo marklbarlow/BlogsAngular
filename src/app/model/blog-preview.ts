ebb691782423d113a0bc9f82e8a6d9f70bc2f0e2
marklbarlow <23555996+marklbarlow@users.noreply.github.com>
2022-11-19 09:44:28 +0000

    Update preview

--- src/app/model/blog-preview.ts
+++ src/app/model/blog-preview.ts
@@ -1,6 +1,5 @@
 export interface BlogPreview {
   id: number;
-  text: string;
   timestamp: string;
   title: string;
   userId: number;
--- src/testing/blog-preview.ts
+++ src/testing/blog-preview.ts
@@ -3,7 +3,6 @@ import { BlogPreview } from 'app/model';
 export const previews: BlogPreview[] = [
   {
     id: 1,
-    text: 'This is some text',
     timestamp: new Date().toISOString(),
     title: 'Proin ac congue lorem, rhoncus',
     userId: 1,
@@ -11,7 +10,6 @@ export const previews: BlogPreview[] = [
   },
   {
     id: 2,
-    text: 'This is some text',
     timestamp: new Date().toISOString(),
     title: 'Donec euismod est vitae nisi',
     userId: 1,
@@ -19,7 +17,6 @@ export const previews: BlogPreview[] = [
   },
   {
     id: 3,
-    text: 'This is some text',
     timestamp: new Date().toISOString(),
     title: 'Maecenas quam urna, auctor eu',
     userId: 1,
