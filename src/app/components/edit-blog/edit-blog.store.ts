c78fd29e7b58ec1e69eb6c79a92724f668fd23a8
Mark Barlow <23555996+marklbarlow@users.noreply.github.com>
2022-11-20 21:15:41 +0000

    Save blog as current user

--- src/app/components/edit-blog/edit-blog.store.ts
+++ src/app/components/edit-blog/edit-blog.store.ts
@@ -2,7 +2,10 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { ComponentStore, tapResponse } from '@ngrx/component-store';
+import { concatLatestFrom } from '@ngrx/effects';
+import { select, Store } from '@ngrx/store';
 import { BlogsService } from 'app/services';
+import { selectSelectedUser } from 'app/store';
 import { mergeMap, Observable } from 'rxjs';
 
 export interface BlogPost {
@@ -14,21 +17,28 @@ export interface BlogPost {
 export class EditBlogStore extends ComponentStore<{}> {
   private save = this.effect((post$: Observable<BlogPost>) =>
     post$.pipe(
-      mergeMap(post =>
-        this.service.saveBlogEntry(post.title, post.content, 1).pipe(
-          tapResponse(
-            _ => {
-              console.log(`Blog post ${post.title} saved succesfully`);
-              this.router.navigate(['/']);
-            },
-            (error: HttpErrorResponse) => console.log(error)
+      concatLatestFrom(() => this.store.pipe(select(selectSelectedUser))),
+      mergeMap(([post, user]) =>
+        this.service
+          .saveBlogEntry(post.title, post.content, user?.id ?? 1)
+          .pipe(
+            tapResponse(
+              _ => {
+                console.log(`Blog post ${post.title} saved succesfully`);
+                this.router.navigate(['/']);
+              },
+              (error: HttpErrorResponse) => console.log(error)
+            )
           )
-        )
       )
     )
   );
 
-  constructor(private router: Router, private service: BlogsService) {
+  constructor(
+    private router: Router,
+    private service: BlogsService,
+    private store: Store
+  ) {
     super();
   }
 
